e028a66ca03de2badd0264cde2eb283c6188508d
Joanna <joannakhek@gmail.com>
2021-08-04 13:27:20 +0800

    added version

--- application.py
+++ application.py
@@ -16,7 +16,7 @@ import plotly.io as pio
 import seaborn as sns
 import pygal
 from pygal.style import Style
-from wordcloud import WordCloud, ImageColorGenerator
+from wordcloud import WordCloud
 from PIL import Image
 from IPython.display import display, HTML
 from sklearn.feature_extraction import text 
